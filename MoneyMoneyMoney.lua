7bcba26383e15dc65ba271a513c05548f95500db
Bjørn Tore Håvie <itsbth@itsbth.com>
2014-04-05 21:52:45 +0200

    I have no idea what I've done

--- MoneyMoneyMoney.lua
+++ MoneyMoneyMoney.lua
@@ -1,11 +1,165 @@
-local CurrentGold = 0
+local GetAddOnMetadata, CreateFrame, GetMoney, RepairAllItems, InRepairMode, MoneyMoneyMoneyDB, LibStub
+do
+  local _obj_0 = _G
+  GetAddOnMetadata, CreateFrame, GetMoney, RepairAllItems, InRepairMode, MoneyMoneyMoneyDB, LibStub = _obj_0.GetAddOnMetadata, _obj_0.CreateFrame, _obj_0.GetMoney, _obj_0.RepairAllItems, _obj_0.InRepairMode, _obj_0.MoneyMoneyMoneyDB, _obj_0.LibStub
+end
+local LibDataBroker = LibStub:GetLibrary("LibDataBroker-1.1")
+MoneyMoneyMoneyDB = MoneyMoneyMoneyDB or { }
+local HierarchicalDB
+do
+  local _base_0 = { }
+  _base_0.__index = _base_0
+  local _class_0 = setmetatable({
+    __init = function(self, levels, queriable)
+      self.levels, self.queriable = levels, queriable
+    end,
+    __base = _base_0,
+    __name = "HierarchicalDB"
+  }, {
+    __index = _base_0,
+    __call = function(cls, ...)
+      local _self_0 = setmetatable({}, _base_0)
+      cls.__init(_self_0, ...)
+      return _self_0
+    end
+  })
+  _base_0.__class = _class_0
+  HierarchicalDB = _class_0
+end
 MoneyMoneyMoney = {
   Name = "MoneyMoneyMoney",
   Version = GetAddOnMetadata("KillTrack", "Version"),
   Frame = CreateFrame("Frame"),
+  Broker = nil,
+  DB = nil,
   Events = {
-    PLAYER_MONEY = function()
-      return print("MMM: Got " .. tostring(CurrentGold()))
+    PLAYER_ENTERING_WORLD = function(self)
+      self.CurrentGold = GetMoney()
+    end,
+    PLAYER_MONEY = function(self)
+      local delta = GetMoney() - self.CurrentGold
+      self.CurrentGold = GetMoney()
+      return self:UpdateMoney(delta)
     end
-  }
+  },
+  OnEvent = function(self, _, evt, ...)
+    if self.Events[evt] then
+      return self.Events[evt](self, ...)
+    else
+      return print("Unhandled event " .. tostring(evt) .. "!")
+    end
+  end,
+  UpdateMoney = function(self, delta)
+    if self.Expected[1] and delta == self.Expected[1].Amount then
+      print("MMM: Expected: " .. tostring(delta) .. " from " .. tostring(self.Expected[1].Category))
+      return table.remove(self.Expected, 1)
+    else
+      for category, opts in pairs(self.Categories) do
+        do
+          local flag = opts.HasFlag
+          if flag then
+            if self.Flags[flag] then
+              print("MMM: Flag: " .. tostring(delta) .. " from " .. tostring(source))
+              return 
+            end
+          end
+        end
+      end
+      return print("MMM: Got " .. tostring(delta) .. " from unknown source.")
+    end
+  end,
+  Expect = function(self, source, amount)
+    self.Expected[#self.Expected + 1] = {
+      Source = source,
+      Amount = amount
+    }
+    return print("MMM: Expecting " .. tostring(amount) .. " from " .. tostring(source))
+  end,
+  CurrentGold = 0,
+  Categories = { },
+  Flags = { },
+  Expected = { },
+  Timers = { },
+  RegisterCategory = function(self, name, opts)
+    if opts == nil then
+      opts = { }
+    end
+    self.Categories[name] = opts
+  end,
+  RegisterEvent = function(self, name, actions)
+    self.Events[name] = function(self, ...)
+      do
+        local flag = actions.SetFlag
+        if flag then
+          self.Flags[flag] = true
+        end
+      end
+      do
+        local flag = actions.ClearFlag
+        if flag then
+          self.Flags[flag] = nil
+        end
+      end
+      do
+        local timer = actions.SetTimer
+        if timer then
+          self.Timers[timer] = GetTime()
+        end
+      end
+    end
+  end,
+  RegisterHook = function(self, name, fun)
+    return hooksecurefunc(name, function(...)
+      return fun(self, ...)
+    end)
+  end
 }
+local MMM = MoneyMoneyMoney
+do
+  local _with_0 = LibDataBroker:NewDataObject("MoneyMoneyMoney_Broker", {
+    type = "data source",
+    label = "MoneyMoneyMoney",
+    tocname = "MoneyMoneyMoney"
+  })
+  _with_0.OnTooltipShow = function(self)
+    return self:AddLine("This is  a test.")
+  end
+  _with_0.text = "MMM: 10|TInterface\\MONEYFRAME\\UI-GoldIcon:0|t"
+  MMM.Broker = _with_0
+end
+MMM:RegisterCategory('Vendor', {
+  HasFlag = 'UsingVendor'
+})
+MMM:RegisterEvent('MERCHANT_SHOW', {
+  SetFlag = 'UsingVendor'
+})
+MMM:RegisterEvent('MERCHANT_CLOSED', {
+  ClearFlag = 'UsingVendor'
+})
+MMM:RegisterHook('BuyMerchantItem', function(self, idx, qty)
+  local _, price, defaultQty
+  _, _, price, defaultQty = GetMerchantItemInfo(idx)
+  return self:Expect('Vendor', -price * (qty or defaultQty))
+end)
+MMM:RegisterCategory('Flights', {
+  EXPENSES = EXPENSES
+})
+MMM:RegisterHook('TakeTaxiNode', function(self, id)
+  return self:Expect('Flights', -TaxiNodeCost(id))
+end)
+MMM:RegisterCategory('Repair')
+MMM:RegisterHook('RepairAllItems', function(self, guildBankRepair)
+  if not (guildBankRepair) then
+    return self:Expect('Repair', -GetRepairAllCost())
+  end
+end)
+for k, _ in pairs(MMM.Events) do
+  MMM.Frame:RegisterEvent(k)
+end
+return MMM.Frame:SetScript("OnEvent", (function()
+  local _base_0 = MMM
+  local _fn_0 = _base_0.OnEvent
+  return function(...)
+    return _fn_0(_base_0, ...)
+  end
+end)())
--- MoneyMoneyMoney.moon
+++ MoneyMoneyMoney.moon
@@ -1,11 +1,96 @@
+import GetAddOnMetadata, CreateFrame, GetMoney,
+  RepairAllItems, InRepairMode,
+  MoneyMoneyMoneyDB, LibStub from _G
+
+LibDataBroker = LibStub\GetLibrary "LibDataBroker-1.1"
+
 export MoneyMoneyMoney
 
-CurrentGold = 0
+MoneyMoneyMoneyDB or= {}
+
+class HierarchicalDB
+  new: (@levels, @queriable) =>
 
 MoneyMoneyMoney =
   Name: "MoneyMoneyMoney"
   Version: GetAddOnMetadata("KillTrack", "Version")
   Frame: CreateFrame "Frame"
+  Broker: nil
+  DB: nil
   Events:
-    PLAYER_MONEY: () ->
-      print "MMM: Got #{CurrentGold()}"
+    PLAYER_ENTERING_WORLD: () =>
+      @CurrentGold = GetMoney()
+    PLAYER_MONEY: () =>
+      delta = GetMoney() - @CurrentGold
+      @CurrentGold = GetMoney()
+      @UpdateMoney(delta)
+
+  OnEvent: (_, evt, ...) =>
+    if @Events[evt]
+      @Events[evt](@, ...)
+    else
+      print "Unhandled event #{evt}!"
+
+  UpdateMoney: (delta) =>
+    if @Expected[1] and delta == @Expected[1].Amount
+      print "MMM: Expected: #{delta} from #{@Expected[1].Category}"
+      table.remove(@Expected, 1)
+    else
+      for category, opts in pairs(@Categories)
+        if flag = opts.HasFlag
+          if @Flags[flag]
+            print "MMM: Flag: #{delta} from #{source}"
+            return
+      print "MMM: Got #{delta} from unknown source."
+
+  Expect: (source, amount) =>
+    @Expected[#@Expected + 1] = Source: source, Amount: amount
+    print "MMM: Expecting #{amount} from #{source}"
+
+  CurrentGold: 0
+  Categories: {}
+  Flags: {}
+  Expected: {}
+  Timers: {}
+
+  RegisterCategory: (name, opts={}) =>
+    @Categories[name] = opts
+
+  RegisterEvent: (name, actions) =>
+    @Events[name] = (...) =>
+      if flag = actions.SetFlag
+        @Flags[flag] = true
+      if flag = actions.ClearFlag
+        @Flags[flag] = nil
+      if timer = actions.SetTimer
+        @Timers[timer] = GetTime()
+
+  RegisterHook: (name, fun) =>
+    hooksecurefunc(name, (...) -> fun(@, ...))
+
+MMM = MoneyMoneyMoney
+
+MMM.Broker = with LibDataBroker\NewDataObject("MoneyMoneyMoney_Broker", type: "data source", label: "MoneyMoneyMoney", tocname: "MoneyMoneyMoney")
+  .OnTooltipShow = () =>
+    @AddLine("This is  a test.")
+  .text = "MMM: 10|TInterface\\MONEYFRAME\\UI-GoldIcon:0|t"
+
+MMM\RegisterCategory 'Vendor', HasFlag: 'UsingVendor'
+MMM\RegisterEvent 'MERCHANT_SHOW', SetFlag: 'UsingVendor'
+MMM\RegisterEvent 'MERCHANT_CLOSED', ClearFlag: 'UsingVendor'
+MMM\RegisterHook 'BuyMerchantItem', (idx, qty) =>
+  _, _, price, defaultQty = GetMerchantItemInfo(idx)
+  @Expect 'Vendor', -price * (qty or defaultQty)
+
+MMM\RegisterCategory 'Flights', :EXPENSES
+MMM\RegisterHook 'TakeTaxiNode', (id) =>
+  @Expect 'Flights', -TaxiNodeCost(id)
+
+MMM\RegisterCategory 'Repair'
+MMM\RegisterHook 'RepairAllItems', (guildBankRepair) =>
+  @Expect 'Repair', -GetRepairAllCost() unless guildBankRepair
+
+for k, _ in pairs(MMM.Events)
+  MMM.Frame\RegisterEvent(k)
+
+MMM.Frame\SetScript "OnEvent", MMM\OnEvent
